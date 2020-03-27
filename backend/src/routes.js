1ec5fbea0ab11330fd8f64021a078487288127ab
juancarlosbs <juanrov_@hotmail.com>
2020-03-26 21:57:08 -0300

    create controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
 const routes = express.Router();
 
-app.post('/users', (req,res) => {
-    const body = req.body;
+routes.post('/sessions', SessionController.create);
 
-    console.log(body);
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-    return res.json({
-        evento : 'Semana OmniStack 11.0',
-        aluno : 'Juan Carlos'
-    })
-});
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
