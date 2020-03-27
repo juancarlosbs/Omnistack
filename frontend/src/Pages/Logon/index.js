0721bcc8d6e4781a75489597f30f21cb81b6c537
juancarlosbs <juanrov_@hotmail.com>
2020-03-27 18:25:16 -0300

    create register form

--- frontend/src/Pages/Logon/index.js
+++ frontend/src/Pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
@@ -17,7 +18,10 @@ export default function Logon() {
                     <input placeholder="Sua ID"/>
                     <button className="button"type="submit">Entrar</button>
 
-                    <a href="/register"><FiLogIn size={16} color="#E02041"/>Não tenho cadastro</a>
+                    <Link className="back-link"to="/register">
+                        <FiLogIn size={16} color="#E02041"/>
+                        Não tenho cadastro
+                        </Link>
                 </form>
             </section>
             <img src={heroesIMG} alt="Heroes" />
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './Pages/Logon';
+import Register from './Pages/Register';
+import Profile from './Pages/Profile';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={Logon}/>
+                <Route path="/" exact component={Logon}/>
+                <Route path="/register" component={Register}/>
+                <Route path="/profile" component={Profile}/>
             </Switch>
         </BrowserRouter>
     );
