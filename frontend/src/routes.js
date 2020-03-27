801ee1cb35a799ab1ae6fc627687187e3d14f22e
juancarlosbs <juanrov_@hotmail.com>
2020-03-27 19:02:21 -0300

    complete sttatic pages

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './Pages/Logon';
 import Register from './Pages/Register';
 import Profile from './Pages/Profile';
+import NewIncident from './Pages/NewIncident';
 
 export default function Routes() {
     return (
@@ -12,6 +13,7 @@ export default function Routes() {
                 <Route path="/" exact component={Logon}/>
                 <Route path="/register" component={Register}/>
                 <Route path="/profile" component={Profile}/>
+                <Route path="/incidents/new" component={NewIncident}/>
             </Switch>
         </BrowserRouter>
     );
